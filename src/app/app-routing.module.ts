e467df9656978ac0ab02601ed26fae28c3cf82a8
Bilel Chniti <bilel.chniti@esprit.tn>
2024-02-26 12:41:53 +0100

    integration template front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './components/banner/banner.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ShortCodeComponent } from './components/short-code/short-code.component';
+import { HourComponent } from './components/hour/hour.component';
+import { DancerComponent } from './components/dancer/dancer.component';
+import { StudentComponent } from './components/student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'banner', component: BannerComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'short-code', component: ShortCodeComponent},
+  { path: 'hour', component: HourComponent},
+  { path: 'dancer', component: DancerComponent},
+  { path: 'student', component: StudentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
